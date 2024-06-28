130f2fe253f4f4e4d4ea06f27714bf1a01609735
seungyongI <lsy03108@gmail.com>
2024-06-28 18:12:34 +0900

    0628

---
+++ java work/Ex01/src/ex03/homework.java
@@ -0,0 +1,25 @@
+package ex03;
+
+public class homework {
+	public static void main(String[] args) {
+		int a = 100;
+		int b = a + 35;
+		double c = b + 3.6;
+		
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(a + b);
+		System.out.println(c);
+		System.out.println(b / a);
+		System.out.println(b % a);
+		
+		boolean d = true;
+		System.out.println(a>b);
+		System.out.println(b<c);
+		System.out.println(a<c);
+		System.out.println(d);
+		System.out.println(Math.pow(a, 2));
+		
+
+	}
+}
