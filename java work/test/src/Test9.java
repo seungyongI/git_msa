5c5efa6ae9a4e8747cd5dcbb08d1e109b2e86f2b
DESKTOP-RCUA7GL\tmddy <lsy03108@gmail.com>
2024-07-19 00:44:13 +0900

    0718

--- java work/test/src/Test9.java
+++ java work/test/src/Test9.java
@@ -2,12 +2,23 @@ public class Test9 {
 
     public static int solution(String ineq, String eq, int n, int m) {
         int answer = 0;
+        if (ineq.equals("<")) {
+            if (eq.equals("=")) {
+                answer = n <= m ? 1 : 0;
+            } else {
+                answer = n < m ? 1 : 0;
+            }
+        } else if (ineq.equals(">")) {
+            if (eq.equals("=")) {
+                answer = n >= m ? 1 : 0;
+            } else {
+                answer = n > m ? 1 : 0;
+            }
+        }
 
-        
         return answer;
     }
 
     public static void main(String[] args) {
-
     }
 }
