e05ae8c9c2805cc6e173bd5772b3730a37d04f91
seungyonglee <lsy03108@gmail.com>
2024-07-25 18:00:34 +0900

    0725

--- CS work/WindowsFormsApp2/WindowsFormsApp2/Form1.Designer.cs
+++ CS work/WindowsFormsApp2/WindowsFormsApp2/Form1.Designer.cs
@@ -40,7 +40,7 @@
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("굴림", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.button1.Location = new System.Drawing.Point(327, 319);
+            this.button1.Location = new System.Drawing.Point(183, 334);
             this.button1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(237, 43);
@@ -58,8 +58,8 @@
             this.label1.Location = new System.Drawing.Point(63, 29);
             this.label1.Margin = new System.Windows.Forms.Padding(20);
             this.label1.Name = "label1";
-            this.label1.Padding = new System.Windows.Forms.Padding(30);
-            this.label1.Size = new System.Drawing.Size(167, 101);
+            this.label1.Padding = new System.Windows.Forms.Padding(20, 30, 20, 30);
+            this.label1.Size = new System.Drawing.Size(147, 101);
             this.label1.TabIndex = 1;
             this.label1.Text = "label1";
             this.label1.Click += new System.EventHandler(this.label1_Click);
@@ -73,8 +73,8 @@
             this.label2.Location = new System.Drawing.Point(230, 29);
             this.label2.Margin = new System.Windows.Forms.Padding(20);
             this.label2.Name = "label2";
-            this.label2.Padding = new System.Windows.Forms.Padding(30);
-            this.label2.Size = new System.Drawing.Size(167, 101);
+            this.label2.Padding = new System.Windows.Forms.Padding(20, 30, 20, 30);
+            this.label2.Size = new System.Drawing.Size(147, 101);
             this.label2.TabIndex = 2;
             this.label2.Text = "label2";
             // 
@@ -87,8 +87,8 @@
             this.label3.Location = new System.Drawing.Point(397, 29);
             this.label3.Margin = new System.Windows.Forms.Padding(20);
             this.label3.Name = "label3";
-            this.label3.Padding = new System.Windows.Forms.Padding(30);
-            this.label3.Size = new System.Drawing.Size(167, 101);
+            this.label3.Padding = new System.Windows.Forms.Padding(20, 30, 20, 30);
+            this.label3.Size = new System.Drawing.Size(147, 101);
             this.label3.TabIndex = 3;
             this.label3.Text = "label3";
             this.label3.Click += new System.EventHandler(this.label3_Click);
@@ -102,8 +102,8 @@
             this.label4.Location = new System.Drawing.Point(63, 170);
             this.label4.Margin = new System.Windows.Forms.Padding(20);
             this.label4.Name = "label4";
-            this.label4.Padding = new System.Windows.Forms.Padding(30);
-            this.label4.Size = new System.Drawing.Size(167, 101);
+            this.label4.Padding = new System.Windows.Forms.Padding(20, 30, 20, 30);
+            this.label4.Size = new System.Drawing.Size(147, 101);
             this.label4.TabIndex = 4;
             this.label4.Text = "label4";
             // 
@@ -116,8 +116,8 @@
             this.label5.Location = new System.Drawing.Point(230, 170);
             this.label5.Margin = new System.Windows.Forms.Padding(20);
             this.label5.Name = "label5";
-            this.label5.Padding = new System.Windows.Forms.Padding(30);
-            this.label5.Size = new System.Drawing.Size(167, 101);
+            this.label5.Padding = new System.Windows.Forms.Padding(20, 30, 20, 30);
+            this.label5.Size = new System.Drawing.Size(147, 101);
             this.label5.TabIndex = 5;
             this.label5.Text = "label5";
             // 
@@ -130,8 +130,8 @@
             this.label6.Location = new System.Drawing.Point(397, 170);
             this.label6.Margin = new System.Windows.Forms.Padding(20);
             this.label6.Name = "label6";
-            this.label6.Padding = new System.Windows.Forms.Padding(30);
-            this.label6.Size = new System.Drawing.Size(167, 101);
+            this.label6.Padding = new System.Windows.Forms.Padding(20, 30, 20, 30);
+            this.label6.Size = new System.Drawing.Size(147, 101);
             this.label6.TabIndex = 6;
             this.label6.Text = "label6";
             // 
@@ -140,13 +140,13 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(601, 390);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.button1);
             this.Font = new System.Drawing.Font("함초롬돋움", 8.999999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
             this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "Form1";
--- CS work/WindowsFormsApp2/WindowsFormsApp2/Form1.cs
+++ CS work/WindowsFormsApp2/WindowsFormsApp2/Form1.cs
@@ -19,8 +19,45 @@ namespace WindowsFormsApp2
 
         private void button1_Click(object sender, EventArgs e)
         {
-            Console.WriteLine("잘 나오니?");
-            label1.Text = "바뀜";
+            Random ran = new Random();
+
+            int num1 = ran.Next(47);
+            int num2 = ran.Next(47);
+            int num3 = ran.Next(47);
+            int num4 = ran.Next(47);
+            int num5 = ran.Next(47);
+            int num6 = ran.Next(47);
+            
+            Console.WriteLine("num1 = " + num1);
+            Console.WriteLine("num2 = " + num2);
+            Console.WriteLine("num3 = " + num3);
+            Console.WriteLine("num4 = " + num4);
+            Console.WriteLine("num5 = " + num5);
+            Console.WriteLine("num6 = " + num6);
+
+            label1.Text = num1.ToString();
+            label1.AutoSize = false;
+            label1.Size = new System.Drawing.Size(147, 101);
+
+            label2.Text = num2.ToString();
+            label2.AutoSize = false;
+            label2.Size = new System.Drawing.Size(147, 101);
+
+            label3.Text = num3.ToString();
+            label3.AutoSize = false;
+            label3.Size = new System.Drawing.Size(147, 101);
+
+            label4.Text = num4.ToString();
+            label4.AutoSize = false;
+            label4.Size = new System.Drawing.Size(147, 101);
+
+            label5.Text = num5.ToString();
+            label5.AutoSize = false;
+            label5.Size = new System.Drawing.Size(147, 101);
+
+            label6.Text = num6.ToString();
+            label6.AutoSize = false;
+            label6.Size = new System.Drawing.Size(167, 101);
         }
 
         private void label1_Click(object sender, EventArgs e)
